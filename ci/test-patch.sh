6ec09096e3c22e611351f5f1469df1dad18343ab
bodand <bodand@proton.me>
2023-03-01 19:30:06 +0100

    feat: add configuration tests on unices
    - install cmake and g++ from relevant packages
    - try to configure the patched files and see if they error

--- ci/test-patch.sh
+++ ci/test-patch.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 
 git clone "$1" "$2"
-cd "$2"
+cd "$2" || exit 1
 git apply --reject --ignore-space-change --ignore-whitespace --whitespace=fix "../$2.patch"
+
+if [ "x$2" = "xlab_01" ]
+then
+  cd nagyobb
+fi
+
+cmake -S. -B_build
