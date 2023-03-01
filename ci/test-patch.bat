82f20ad9c9df98569ce65f7970939464af34a9d8
bodand <bodand@proton.me>
2023-03-01 21:08:13 +0100

    fix(ci): fix windows-msys2 to use ninja generator instead

--- ci/test-patch.bat
+++ ci/test-patch.bat
@@ -5,6 +5,7 @@ git apply --reject --ignore-space-change --ignore-whitespace --whitespace=fix ..
 if "%~2" == "lab_01" cd nagyobb
 
 if "%DO_MINGW%" == "yes" set "cmake_gen=-G "MinGW Makefiles""
+if "%DO_MSYS2%" == "yes" set "cmake_gen=-G Ninja"
 if "%DO_MSVC%" == "yes" set "cmake_gen=-G "Visual Studio 17 2022""
 
 cmake -S. -B_build %cmake_gen%
